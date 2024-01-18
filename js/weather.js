4098fa93a032a5f5dab39136374d26ca03192ccc
AFelixLiu <yzpkqdkl@gmail.com>
2024-01-18 17:05:12 +0800

    Site updated: 2024-01-18 17:05:08

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "d08635",
+    "tmpSize": "16",
+    "cityColor": "f5222d",
+    "citySize": "16",
+    "aqiColor": "2f54eb",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "0",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "210",
+    "top": "20",
+    "key": "6451bfb431d548359a5c7670acea97f4"
+  }
+}
